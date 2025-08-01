f02fb89b2f8baa74d4a376779f087e34c014eae6
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-08-01 15:08:00 +0500

    trap

--- Solutions/hard.py
+++ Solutions/hard.py
@@ -1,8 +1,31 @@
 from collections import defaultdict
 
 class Solution:
-    pass 
+    def trap(self, height: list[int]) -> int:
+        n = len(height)
+        if n == 0:
+            return 0
+        
+        prefix = [None] * n
+        suffix = [None] * n
 
+        prefix[0] = height[0]
+        for i in range(1, n):
+            prefix[i] = max(prefix[i-1], height[i])
+        
+        suffix[n-1] = height[n-1]
+        for i in range(n-2, -1, -1):
+            suffix[i] = max(suffix[i+1], height[i])
+
+        water = 0
+        for i in range(n):
+            water_i = (min(prefix[i], suffix[i]) - height[i]) 
+            if water_i > 0:
+                water += water_i 
+
+        return water 
 
 
 ans = Solution()
+#print(ans.trap([0,2,0,3,1,0,1,3,2,1]))
+print(ans.trap([1,5,2,3,4]))
