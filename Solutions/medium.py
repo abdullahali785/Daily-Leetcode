bc35a5f0c1caca9ebbffeeea5b565662842a91d7
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-08-09 14:19:21 +0500

    carFleet

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -196,5 +196,15 @@ class Solution:
 
         return res 
 
+    def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        pair = [[p, s] for p, s in zip(position, speed)]
+
+        stack = []
+        for p, s in sorted(pair)[::-1]:
+            stack.append((target - p) / s)
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+
 ans = Solution()
 print(ans.dailyTemperatures([30,38,30,36,35,40,28]))
