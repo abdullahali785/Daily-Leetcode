5b65868431c6b10a7fd26c6fac7083fc07a146ee
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2025-08-17 13:58:02 +0500

    maxSlidingWindow

--- Solutions/hard.py
+++ Solutions/hard.py
@@ -1,4 +1,6 @@
+import collections
 from collections import defaultdict
+from collections import deque
 
 class Solution:
     def trap(self, height: list[int]) -> int:
@@ -74,7 +76,27 @@ class Solution:
         l, r = res 
         return s[l: r+1] if resLen != float("infinity") else ""
 
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        res = []
+        q = collections.deque()
+        l = r = 0
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                res.append(nums[q[0]])
+                l += 1
+
+            r += 1
+
+        return res 
+            
 
 ans = Solution()
-#print(ans.trap([0,2,0,3,1,0,1,3,2,1]))
-print(ans.trap([1,5,2,3,4]))
+print(ans.maxSlidingWindow([1,2,1,0,4,2,6], 3))
