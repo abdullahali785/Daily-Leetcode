1163fffd2096c389761e16661ae232b4813fa9f3
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-02-07 20:23:27 -0600

    encode decode

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -146,6 +146,27 @@ class Solution:
                 ans.append(n)
                 if len(ans) == k:
                     return ans
+                
+    def encode(self, strs: list[str]) -> str:
+        res = ""
+        for string in strs:
+            res += str(len(string)) + "#" + string
+
+        return res
+
+    def decode(self, s: str) -> list[str]:
+        res, i = [], 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+
+            length = int(s[i : j])
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+
+        return res 
 
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         n = len(nums)
