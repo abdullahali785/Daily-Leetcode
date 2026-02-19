19df5a51121967e8678c922ea40b66a908d96977
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-02-18 20:57:36 -0600

    trap rain water 2 pointer

--- Solutions/hard.py
+++ Solutions/hard.py
@@ -41,6 +41,26 @@ class Solution:
 
         return water 
     
+    def trap_2pointer(self, height: list[int]) -> int:
+        if not height:
+            return 0
+
+        l, r = 0, len(height)-1
+        l_max, r_max = height[l], height[r]
+        res = 0
+
+        while l < r:
+            if l_max < r_max:
+                l += 1
+                l_max = max(l_max, height[l])
+                res += l_max - height[l]
+            else:
+                r -= 1
+                r_max = max(r_max, height[r])
+                res += r_max - height[r]
+        
+        return res 
+    
     def largestRectangleArea(self, heights: list[int]) -> int:
         maxArea = 0
         stack = []
