656860ea11585614274615dcd97c8b727311cc15
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-03-01 21:04:42 -0600

    car fleet

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -390,15 +390,16 @@ class Solution:
             stack.append([t, i])
             
         return res
-
+    
     def carFleet(self, target: int, position: list[int], speed: list[int]) -> int:
+        stack = []
         pair = [[p, s] for p, s in zip(position, speed)]
 
-        stack = []
         for p, s in sorted(pair)[::-1]:
             stack.append((target - p) / s)
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
+
         return len(stack)
     
     def maxProfit(self, prices: list[int]) -> int:
