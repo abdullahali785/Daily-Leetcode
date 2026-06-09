1c62d87541273803f6a5e65f7e964795b5e5e0b3
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-06-09 14:40:28 +0500

    topKFrequent_bucket2

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -122,7 +122,6 @@ class Solution:
             hashmap[tuple(frequency)].append(string)
 
         return list(hashmap.values())
-    
 
     def topKFrequent(self, nums: list[int], k: int) -> list[int]:
         count = defaultdict(int)
@@ -155,6 +154,23 @@ class Solution:
                 ans.append(n)
                 if len(ans) == k:
                     return ans
+
+    def topKFrequent_bucket2(self, nums: list[int], k: int) -> list[int]:    
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        for num, count in count.items():
+            freq[count].append(num)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
                 
     def encode(self, strs: list[str]) -> str:
         res = ""
