6c6bf17b6d9f2e64193f71a47d6b19ed8806d45a
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-06-10 14:30:48 +0500

    encode decode strs

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -176,22 +176,22 @@ class Solution:
         res = ""
         for string in strs:
             res += str(len(string)) + "#" + string
-
+        
         return res
 
     def decode(self, s: str) -> list[str]:
-        res, i = [], 0
+        result, i = [], 0
 
         while i < len(s):
             j = i
             while s[j] != "#":
                 j += 1
 
-            length = int(s[i : j])
-            res.append(s[j + 1 : j + 1 + length])
-            i = j + 1 + length
+            lenght = int(s[i:j])
+            result.append(s[j+1 : j + 1 + lenght])
+            i = j + 1 + lenght
 
-        return res  
+        return result 
     
     def productExceptSelf(self, nums: list[int]) -> list[int]:
         prefix, sufix, res = [], [], []
