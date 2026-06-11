c470b29c4a5b03e3fcb7d0a4ef22a406672ffc2f
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-06-11 15:19:48 +0500

    productExceptSelf_lowMemory

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -217,6 +217,20 @@ class Solution:
 
         return res 
     
+    def productExceptSelf_lowMemory(self, nums: list[int]) -> list[int]:
+        res = [1] * len(nums)
+        pre, post = 1, 1
+
+        for i in range(len(nums)):
+            res[i] = pre 
+            pre *= nums[i]
+
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= post
+            post *= nums[i] 
+
+        return res
+    
     def isValidSudoku(self, board: list[list[str]]) -> bool:
         row = [set() for i in range(9)]
         column = [set() for i in range(9)]
