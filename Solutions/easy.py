26019b3f18c6ebd802b7535feba33f039d322151
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-06-24 14:25:24 +0500

    isPalindrome_pointer'

--- Solutions/easy.py
+++ Solutions/easy.py
@@ -130,6 +130,27 @@ class Solution:
             r -= 1    
 
         return True
+    
+    def isPalindrome_pointer(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while r > l:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False 
+
+            l, r = l + 1, r - 1
+
+        return True
+
+    def alphaNum(self, char: str) -> bool:
+        return (ord('A') <= ord(char) <= ord('Z') or 
+        ord('a') <= ord(char) <= ord('z') or 
+        ord('0') <= ord(char) <= ord('9'))
 
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
