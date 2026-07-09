faf53e6e573b66d232f6661ff1eca074d5bf3128
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-07-09 14:54:33 +0500

    LRU cache

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -1,5 +1,4 @@
-import math
-import collections
+import math, collections
 from typing import Optional
 from collections import Counter, defaultdict
 
@@ -52,35 +51,45 @@ class LRUCache:
         self.cap = capacity
         self.cache = {}
 
-        self.left, self.right = nodeDouble(0,0), nodeDouble(0,0)
+        # left = LRU, right = Recent
+        self.left, self.right = Node(0, 0), Node(0, 0)
         self.left.next, self.right.prev = self.right, self.left
 
+    # Remove from left
     def remove(self, node):
         prev, nxt = node.prev, node.next
         prev.next, nxt.prev = nxt, prev
 
+    # Insert at right
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
         node.next, node.prev = nxt, prev
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+        try:
+            value = self.cache[key]
+            self.remove(value)
+            self.insert(value)
+            return value.val
+        except:
+            return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-        self.cache[key] = nodeDouble(key, value)
+        self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
 
         if len(self.cache) > self.cap:
             lru = self.left.next
             self.remove(lru)
             del self.cache[lru.key]
+
+class LRU_Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.next = self.prev = None
 
 class Solution:
     def groupAnagrams_sorting(self, strs: list[str]) -> list[list[str]]:
