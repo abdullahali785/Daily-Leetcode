c3f1d6a9ea614b49253d551900d04b48d403a466
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-07-11 15:39:25 +0500

    mergeKLists

--- Solutions/hard.py
+++ Solutions/hard.py
@@ -149,13 +149,38 @@ class Solution:
         return dummy.next
     
     def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
-        if len(lists) == 0:
+        if not lists or len(lists) == 0:
             return None
-            
-        for i in range(1, len(lists)):
-            lists[i] = self.mergeTwoLists(lists[i], lists[i-1])
 
-        return lists[-1]
+        while len(lists) > 1:
+            mergedLists = []
+
+            for i in range(0, len(lists), 2):
+                l1 = lists[i]
+                l2 = lists[i+1] if (i + 1) < len(lists) else None
+                mergedLists.append(self.mergeList(l1, l2))
+            lists = mergedLists
+
+        return lists[0]
+
+    def mergeList(self, l1, l2):
+        dummy = ListNode()
+        tail = dummy
+
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
     
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
