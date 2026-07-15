526a842c781838bf5ef8bfe62fb2da2f9b7fb0eb
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-07-15 14:37:32 +0500

    characterReplacement

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -511,14 +511,13 @@ class Solution:
 
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
-            
-            if (r - l + 1) - max(count.values()) > k:
+
+            while ((r - l + 1)) - max(count.values()) > k:
                 count[s[l]] -= 1
                 l += 1
 
             res = max(res, (r - l + 1))
-
-        return res 
+        return res  
     
     def checkInclusion(self, s1: str, s2: str) -> bool:
         if len(s1) > len(s2):
