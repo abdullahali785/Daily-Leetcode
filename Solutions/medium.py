cb0036f71437438179f0d1c2207bdeda651a82ec
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-07-28 14:40:30 +0500

    findMin

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -745,19 +745,18 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
-            if nums[l] <= nums[r]:
-                res = min(res, nums[l])
-                break
+            if nums[l] < nums[r]:
+                return min(res, nums[l])
+                
+            m = (l + r) // 2
+            res = min(res, nums[m])
 
-            mid = (l + r) // 2
-            res = min(res, nums[mid])
-
-            if nums[l] <= nums[mid]:
-                l = mid + 1
+            if nums[m] >= nums[l]:
+                l = m + 1
             else:
-                r = mid - 1
+                r = m - 1
 
-        return res
+        return res 
 
 ans = Solution()
 print(ans.removeNthFromEnd([1,2,3,4], 2))
