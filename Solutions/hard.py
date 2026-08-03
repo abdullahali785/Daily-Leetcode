c5aef81b9598eed17ce70902a4674af208dd327e
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-08-03 19:26:52 +0300

    findMedianSortedArrays

--- Solutions/hard.py
+++ Solutions/hard.py
@@ -211,6 +211,34 @@ class Solution:
             curr = curr.next
             k -= 1
         return curr
+
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        A, B = nums1, nums2
+        total = len(A) + len(B)
+        half = (total + 1) // 2
+
+        if len(A) > len(B):
+            A, B = B, A
+        
+        l, r = 0, len(A) - 1
+
+        while True:
+            i = (l + r) // 2
+            j = half - i - 2
+
+            ALeft = A[i] if i >= 0 else float('-infinity')
+            ARight = A[i + 1] if (i + 1) < len(A) else float('infinity')
+            BLeft = B[j] if j >= 0 else float('-infinity')
+            BRight = B[j + 1] if (j + 1) < len(B) else float('infinity')
+
+            if ALeft <= BRight and BLeft <= ARight:
+                if total % 2:
+                    return max(ALeft, BLeft)
+                return (max(ALeft, BLeft) + min(ARight, BRight)) / 2
+            elif ALeft > BRight:
+                r = i - 1
+            else:
+                l = i + 1
             
 ans = Solution()
 print(ans.maxSlidingWindow([1,2,1,0,4,2,6], 3))
