d5d00b8b995b691706b53cd6bd17cfce4a2c1302
Abdullah Ali <abdullahali.3.3.2006@gmail.com>
2026-08-31 20:45:04 -0500

    time map

--- Solutions/medium.py
+++ Solutions/medium.py
@@ -98,23 +98,24 @@ class TimeMap:
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.hashmap:
             self.hashmap[key] = []
-
+            
         self.hashmap[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
         res = ""
         values = self.hashmap.get(key, [])
+        l, r = 0, len(values)-1
 
-        l, r = 0, len(values) - 1
         while l <= r:
-            mid = (l + r) // 2
-            if values[mid][1] <= timestamp:
-                res = values[mid][0]
-                l = mid + 1
-            else:
-                r = mid - 1
+            m = (l + r) // 2
 
-        return res 
+            if values[m][1] <= timestamp:
+                res = values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+
+        return res
 
 class Solution:
     def groupAnagrams_sorting(self, strs: list[str]) -> list[list[str]]:
